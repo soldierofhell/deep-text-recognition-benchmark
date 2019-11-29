8b15a4203f362b2b9dda83dda7a1b6142de31548
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-11-29 11:57:44 +0100

    Update transformation.py

--- modules/transformation.py
+++ modules/transformation.py
@@ -80,6 +80,11 @@ class LocalizationNetwork(nn.Module):
         output:    batch_C_prime : Predicted coordinates of fiducial points for input batch [batch_size x F x 2]
         """
         batch_size = batch_I.size(0)
+        
+        zeros = torch.zeros(batch_size, 3, 2, dtype=torch.float, device=batch_C_prime.device)
+        print('localization: ', zeros.device)
+        print('localization: ', self.ctrl_pts_x.device)
+        
         features = self.conv(batch_I).view(batch_size, -1)
         batch_C_prime = self.localization_fc2(self.localization_fc1(features)).view(batch_size, self.F, 2)
         return batch_C_prime
