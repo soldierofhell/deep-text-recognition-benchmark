ee260a94105680629f6a5501d0cc2b0701963216
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-01-24 14:49:54 +0100

    Update prediction.py

--- modules/prediction.py
+++ modules/prediction.py
@@ -34,9 +34,9 @@ class Attention(nn.Module):
         hidden = (torch.FloatTensor(batch_size, self.hidden_size).fill_(0).to(device),
                   torch.FloatTensor(batch_size, self.hidden_size).fill_(0).to(device))
 
-        #print('is_train: ', is_train)
-        
+            
         if is_train:
+            print('is_train: ', is_train)
             for i in range(num_steps):
                 # one-hot vectors for a i-th char. in a batch
                 char_onehots = self._char_to_onehot(text[:, i], onehot_dim=self.num_classes)
